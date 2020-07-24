d9e5932b9a009c2eceb897e5bd7f010b5b959e8b
Huzoor Bux <huzoorbux@gmail.com>
2020-07-24 15:29:32 +0500

    zoom update

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://www.knsinstitute.com/yt.php')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
